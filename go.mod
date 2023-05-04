35d5edf38f20f6be63aec271778e1fc97346bfd4
BRUHITsABunny <53124399+BRUHItsABunny@users.noreply.github.com>
2023-05-04 03:30:37 +0200

    Initial commit

--- go.mod
+++ go.mod
@@ -3,11 +3,18 @@ module github.com/BRUHItsABunny/gOkHttp-advanced-responses
 go 1.19
 
 require (
-	github.com/BRUHItsABunny/gOkHttp v0.2.9
+	github.com/BRUHItsABunny/gOkHttp v0.3.0
 	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/stretchr/testify v1.8.1
 )
 
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.10.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- responses_test.go
+++ responses_test.go
@@ -1,1 +1,26 @@
 package gokhttp_advanced_responses
+
+import (
+	"context"
+	"fmt"
+	gokhttp "github.com/BRUHItsABunny/gOkHttp"
+	gokhttp_requests "github.com/BRUHItsABunny/gOkHttp/requests"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestHTMLParser(t *testing.T) {
+	hClient, err := gokhttp.NewHTTPClient()
+	require.NoError(t, err, "NewHTTPClient: errored unexpectedly.")
+
+	req, err := gokhttp_requests.MakeGETRequest(context.Background(), "https://github.com")
+	require.NoError(t, err, "requests.MakeGETRequest: errored unexpectedly.")
+
+	resp, err := hClient.Do(req)
+	require.NoError(t, err, "hClient.Do: errored unexpectedly.")
+
+	respHTML, err := ResponseHTML(resp)
+	require.NoError(t, err, "ResponseHTML: errored unexpectedly.")
+
+	fmt.Println(respHTML.Text())
+}
