97022a4ad117de332d09c4f0d24df242290bf3d5
BRUHItsABunny <53124399+BRUHItsABunny@users.noreply.github.com>
2024-11-10 01:48:44 -0500

    feat: add dynamic JSON parser

--- go.mod
+++ go.mod
@@ -1,20 +1,21 @@
 module github.com/BRUHItsABunny/gOkHttp-advanced-responses
 
-go 1.19
+go 1.23
 
 require (
-	github.com/BRUHItsABunny/gOkHttp v0.3.0
-	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/BRUHItsABunny/gOkHttp v0.3.7
+	github.com/Jeffail/gabs v1.4.0
+	github.com/PuerkitoBio/goquery v1.10.0
 	github.com/stretchr/testify v1.8.1
 )
 
 require (
-	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.10.0 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- responses.go
+++ responses.go
@@ -1,22 +1,35 @@
 package gokhttp_advanced_responses
 
 import (
-	"bytes"
 	"fmt"
 	gokhttp_responses "github.com/BRUHItsABunny/gOkHttp/responses"
+	"github.com/Jeffail/gabs"
 	"github.com/PuerkitoBio/goquery"
 	"net/http"
 )
 
 func ResponseHTML(resp *http.Response) (*goquery.Document, error) {
-	respBytes, err := gokhttp_responses.ResponseBytes(resp)
-	if err != nil {
-		return nil, fmt.Errorf("responses.ResponseBytes: %w", err)
-	}
-
-	doc, err := goquery.NewDocumentFromReader(bytes.NewBuffer(respBytes))
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
 		return nil, fmt.Errorf("goquery.NewDocumentFromReader: %w", err)
 	}
+
+	err = resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("resp.Body.Close: %w", err)
+	}
 	return doc, err
+}
+
+func ResponseDynamicJSON(resp *http.Response) (*gabs.Container, error) {
+	respBytes, err := gokhttp_responses.ResponseBytes(resp)
+	if err != nil {
+		return nil, fmt.Errorf("gokhttp_responses.ResponseBytes: %w", err)
+	}
+
+	container, err := gabs.ParseJSON(respBytes)
+	if err != nil {
+		return nil, fmt.Errorf("gabs.ParseJSON: %w", err)
+	}
+	return container, err
 }
