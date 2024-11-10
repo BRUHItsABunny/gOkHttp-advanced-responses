24c74a8b259438ed42bb008f7cfa8d3e3687cc6e
BRUHItsABunny <53124399+BRUHItsABunny@users.noreply.github.com>
2024-11-10 17:10:11 -0500

    feat: add JS AST

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/Jeffail/gabs v1.4.0
 	github.com/PuerkitoBio/goquery v1.10.0
 	github.com/stretchr/testify v1.8.1
+	github.com/t14raptor/go-fast v0.0.0-20241021160625-69b37010c3c1
 )
 
 require (
@@ -16,6 +17,6 @@ require (
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.10.0 // indirect
 	golang.org/x/net v0.29.0 // indirect
-	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- responses.go
+++ responses.go
@@ -5,7 +5,10 @@ import (
 	gokhttp_responses "github.com/BRUHItsABunny/gOkHttp/responses"
 	"github.com/Jeffail/gabs"
 	"github.com/PuerkitoBio/goquery"
+	"github.com/t14raptor/go-fast/ast"
+	"github.com/t14raptor/go-fast/parser"
 	"net/http"
+	"strings"
 )
 
 func ResponseHTML(resp *http.Response) (*goquery.Document, error) {
@@ -32,4 +35,44 @@ func ResponseDynamicJSON(resp *http.Response) (*gabs.Container, error) {
 		return nil, fmt.Errorf("gabs.ParseJSON: %w", err)
 	}
 	return container, err
+}
+
+func ResponseAST(resp *http.Response) (*ast.Program, error) {
+	respText, err := gokhttp_responses.ResponseText(resp)
+	if err != nil {
+		return nil, fmt.Errorf("gokhttp_responses.ResponseText: %w", err)
+	}
+
+	astParser, err := parser.ParseFile(respText)
+	if err != nil {
+		return nil, fmt.Errorf("parser.ParseFile: %w", err)
+	}
+
+	return astParser, nil
+}
+
+func ResponseASTFromHTML(resp *http.Response) (*ast.Program, error) {
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("goquery.NewDocumentFromReader: %w", err)
+	}
+
+	err = resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("resp.Body.Close: %w", err)
+	}
+
+	jsSB := strings.Builder{}
+
+	doc.Find("script").Each(func(index int, element *goquery.Selection) {
+		jsSB.WriteString(element.Text())
+		jsSB.WriteString("\n")
+	})
+
+	astParser, err := parser.ParseFile(jsSB.String())
+	if err != nil {
+		return nil, fmt.Errorf("parser.ParseFile: %w", err)
+	}
+
+	return astParser, nil
 }
